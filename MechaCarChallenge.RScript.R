07c9c861e25dd2608a1faa3f197b86037eab9496
fabeza <fabeza22@gmail.com>
2023-02-01 20:05:13 -0800

    challenge deliverables

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,3 +1,5 @@
+# Part 1: Linear Regression to Predict MPG
+
 # Load dplyr package
 library(dplyr)
 
@@ -12,3 +14,29 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance +
 # Determine the p-value and the r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, data=df))
+
+# Part 2: Create Visualizations for the Trip Analysis
+
+# Import csv file as table
+df_coil = read.csv("Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+
+# Create a total_summary table
+total_summary = df_coil %>% summarize(psi_mean=mean(PSI), psi_median=median(PSI),
+                                      psi_variance=var(PSI), psi_sd=sd(PSI))
+
+# Create lot_summary table using group_by()
+lot_summary = df_coil %>% group_by(Manufacturing_Lot) %>% 
+  summarize(psi_mean=mean(PSI), psi_median=median(PSI), psi_variance=var(PSI), 
+            psi_sd=sd(PSI))
+
+# Part 3: T-Tests on Suspension Coils
+?t.test
+# Determine if all manufacturing lots and each lot individually are 
+# statistically different from the population mean of 1,500 pounds psi..
+t.test(df_coil$PSI, mu=1500)
+
+# Determine if the PSI for each manufacturing lot is statistically different 
+# from the population mean of 1,500 pounds psi.
+t.test(subset(df_coil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+t.test(subset(df_coil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+t.test(subset(df_coil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# MechaCar_Statistical_Analysis
+# MechaCar Statistical Analysis
+
+## Linear Regression to Predict MPG
+
+![mpg_linear_regression.png]()
+
+From the linear regression model summary above we can determine the following:
+
+The vehicle length and ground clearance variables provided a non-random amount of variance to the log values in the dataset. In other words, the vehicle length and ground clearance have a significant impact on the vehicle’s fuel efficiency (MPG).
+
+A p-value of 5.35e-11 (lower than the significance level of 0.05) suggests that the slope is not zero.
+
+ An R-squared value of 0.7149 reveals that 71% of the variability observed in the target variable is explained by the linear regression model. Therefore, this model predicts mpg of MechaCar prototypes effectively.
+
+## Summary Statistics on Suspension Coils
+
+The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. The current manufacturing data shows the PSI variance for all manufacturing lots is 62.3 which meets the design specification. 
+
+![total_summary.png]()
+
+When looking at each lot individually, Lot1 and Lot2 are compliant with manufacturing specifications, with a PSI variance of 0.97 and 7.47 respectively. Lot3 has a PSI variance of 170.29, which means it does not meet the manufacturing specifications and must be corrected.
+
+![lot_summary.png]()
+
+## T-Tests on Suspension Coils
